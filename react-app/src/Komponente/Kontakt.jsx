52992f1fd0a4f3892f081dd7e0e205b79068a725
Andrea Stojkov <andreastojkov00@gmail.com>
2023-02-19 11:51:30 +0100

    Napravljena kontakt stranica

---
+++ react-app/src/Komponente/Kontakt.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {useState} from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+function Kontakt() {
+
+
+
+
+    return (
+        <div id='kontakt' >
+
+        </div>
+    )
+}
+
+export default Kontakt;
