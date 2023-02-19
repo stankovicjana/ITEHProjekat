85324b31cb65bc87ac994672ab00e7d436e8ba4b
Andrea Stojkov <andreastojkov00@gmail.com>
2023-02-19 13:07:19 +0100

    Dodatno stilizovanje pocetne strane

--- react-app/src/Komponente/Pocetna.jsx
+++ react-app/src/Komponente/Pocetna.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
+import './Pocetna.css'
 
 
 
 const slideImages = [
-  'https://veritasit.rs/wp-content/uploads/2019/01/prodaja-racunarske-opreme.jpg',
-  'https://www.benchmark.rs/assets/img/news/big_thumb/b9cdc2c04edb8423e8bab23be8c3563d.jpg',
-  'https://slika.nezavisne.rs/2023/01/750x450/20230130072209_756334.jpg'
+  'https://thumbs.dreamstime.com/b/saint-petersburg-russia-circa-may-goods-display-sony-store-galeria-shopping-center-electronics-store-134641471.jpg',
+  'https://honeywell.scene7.com/is/image/honeywell/hon-ab-retail-checkout-2022',
+  'https://im.indiatimes.in/media/content/2013/Jun/ms_1371205962_540x540.jpg'
 ];
 
 const Pocetna = () => {
@@ -18,8 +19,24 @@ const Pocetna = () => {
 
     return (
     
+      
   
       <div> 
+        <div className='main-container'>
+      <image url='https://econsultancy.imgix.net/content/uploads/2019/01/21111315/nike-speed-shop.jpg?auto=compress,format&q=60&w=826&h=546'/>
+      <h1>DOBRODOŠLI U SVET TEHNIKE</h1>
+      <p>Kupite tehniku, najbrze, najbolje, najpovoljnije!</p>
+    </div>
+    <div className='main-text'>
+    <h1>KO SMO MI</h1>
+    <h3>Vasa omiljena prodavnica. Gde je sve dostupno na jednom mestu.</h3>
+      <p>
+      Kupite najbolju tehniku. 
+      </p>
+      <p>
+      Sacuvajte svoje vreme i novac.
+      </p>
+    </div>
         <Slide easing="ease">
           <div className="each-slide">
             <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
